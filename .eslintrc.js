2940db5ad6696b5713b3ee0149e9c09d5932c708
丁铭锋 <CHAD@MBP-DMF.local>
2021-04-10 18:47:54 +0800

    初始化

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,20 +1,67 @@
 module.exports = {
-  root: true,
-  env: {
-    node: true,
-  },
-  extends: [
-    "plugin:vue/essential",
-    "eslint:recommended",
-    "@vue/typescript/recommended",
-    "@vue/prettier",
-    "@vue/prettier/@typescript-eslint",
-  ],
-  parserOptions: {
-    ecmaVersion: 2020,
-  },
-  rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
-  },
-};
+	env: {
+		browser: true,
+		node: true,
+		es2021: true
+	},
+	parserOptions: {
+		sourceType: 'module',
+		ecmaFeatures: {
+			jsx: true
+		}
+	},
+	overrides: [
+		{
+			files: ['*.js', '*.jsx'],
+			extends: ['standard', 'plugin:prettier/recommended', 'prettier/standard']
+		},
+		{
+			files: ['*.ts', '*.tsx'],
+			parser: '@typescript-eslint/parser',
+			parserOptions: {
+				project: './tsconfig.json'
+			},
+			extends: [
+				'standard-with-typescript',
+				'plugin:prettier/recommended',
+				'prettier/standard',
+				'prettier/@typescript-eslint'
+			]
+		},
+		{
+			files: ['*.vue'],
+			// parser: 'vue-eslint-parser',
+			parserOptions: {
+				parser: '@typescript-eslint/parser',
+				extraFileExtensions: ['.vue'],
+				// @typescript-eslint/parser使用
+				project: './tsconfig.json'
+			},
+			extends: [
+				'standard',
+				'plugin:@typescript-eslint/recommended',
+				'plugin:vue/recommended',
+				'plugin:prettier/recommended',
+				'prettier/standard',
+				'prettier/@typescript-eslint',
+				'prettier/vue'
+			]
+		},
+		{
+			files: ['*.html'],
+			extends: ['standard', 'plugin:prettier/recommended', 'prettier/standard'],
+			plugins: ['html'],
+			env: {
+				node: false,
+				es2021: false
+			},
+			parserOptions: {
+				sourceType: 'script',
+				ecmaVersion: 5,
+				ecmaFeatures: {
+					jsx: false
+				}
+			}
+		}
+	]
+}
